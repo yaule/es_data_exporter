3b20adf97567f42c3cc48676acf32b98e52196f3
kasen <lijixing@gmail.com>
2019-01-22 20:26:54 +0800

    update pandas

--- es_data_exporter/collector.py
+++ es_data_exporter/collector.py
@@ -7,55 +7,60 @@ from datetime import datetime
 
 
 def search_es(config, kerberos, tls):
-    pro_rows = '\n'
-    host = config['es_host']
+    pro_rows = "\n"
+    host = config["es_host"]
     try:
         a = None
         if kerberos:
             a = HTTPKerberosAuth()
-        s = ''
+        s = ""
         if tls:
-            s = 's'
-        r = requests.post("http{}://{}/{}/_search".format(s, host, config['es_index']),
-                          headers={"Content-Type": "application/json"},
-                          data=json.dumps(config['es_query']),
-                          auth=a, timeout=10
-                          )
+            s = "s"
+        r = requests.post(
+            "http{}://{}/{}/_search".format(s, host, config["es_index"]),
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(config["es_query"]),
+            auth=a,
+            timeout=10,
+        )
     except:
-        raise Exception('Cannot connect to Elasticsearch host: {}\n{}'.format(
-            host, "http{}://{}/{}/_search".format(s, host, config['es_index'])))
+        raise Exception(
+            "Cannot connect to Elasticsearch host: {}\n{}".format(
+                host, "http{}://{}/{}/_search".format(s, host, config["es_index"])
+            )
+        )
     if r.status_code == 200:
-        es_data = r.json()['hits']['hits']
+        es_data = r.json()["hits"]["hits"]
         es_list = []
         for x in es_data:
-            es_list.append(x.get('_source'))
+            es_list.append(x.get("_source"))
         df = json_normalize(es_list)
         for _, rows in df.iterrows():
+            rows.dropna(axis=1, how="any", inplace=True)
             df_keys = rows.keys()
             row = rows.to_dict()
-            stats_tags = ''
-            for t in config['prometheus_tags']:
+            stats_tags = ""
+            for t in config["prometheus_tags"]:
                 try:
-                    stats_tags += ',{}:"{}"'.format(
-                        t.replace('.', '_'), row[t])
+                    stats_tags += ',{}="{}"'.format(t.replace(".", "_"), row[t])
                 except KeyError:
                     pass
-            stats_tags = '{' + stats_tags[1:] + '}'
-            timestamp = int(datetime.strptime(
-                row[config['timestamp_tag']], '%Y-%m-%dT%H:%M:%S.%f%z').timestamp() * 1000)
+            stats_tags = "{" + stats_tags[1:] + "}"
+            timestamp = int(
+                datetime.strptime(
+                    row[config["timestamp_tag"]], "%Y-%m-%dT%H:%M:%S.%f%z"
+                ).timestamp()
+                * 1000
+            )
             for k in df_keys:
-                if isinstance(row[k], float):
-                    try:
-                        float(row[k])
-                    except ValueError:
-                        break
                 if isinstance(row[k], int) or isinstance(row[k], float):
-                    pro_row = '{}{} {} {}\n'.format(
-                        k.replace('.', '_'), stats_tags, row[k], timestamp)
+                    pro_row = "{}{} {} {}\n".format(
+                        k.replace(".", "_"), stats_tags, row[k], timestamp
+                    )
                     pro_rows = pro_rows + pro_row
                 else:
                     pass
     else:
-        raise Exception('Query failed: {}'.format(r.json()))
+        raise Exception("Query failed: {}".format(r.json()))
 
     return pro_rows.encode()
--- setup.py
+++ setup.py
@@ -10,17 +10,22 @@ setup(
     author_email="kasen@outlook.com",
     description=("Elasticsearch data exporter to the Prometheus ."),
     long_description=(
-        "See https://github.com/yaule/es_data_exporter/blob/master/README.md for documentation."),
+        "See https://github.com/yaule/es_data_exporter/blob/master/README.md for documentation."
+    ),
     license="MIT",
     keywords="prometheus exporter network monitoring elastic search",
     url="https://github.com/yaule/es_data_exporter",
     scripts=["scripts/es_data_exporter"],
     packages=["es_data_exporter"],
-    install_requires=["pyyaml>=3.13",
-                      "requests>=2.21.0", "requests-kerberos>=0.12.0","pandas>=0.23.4"],
+    install_requires=[
+        "pyyaml>=3.13",
+        "requests>=2.21.0",
+        "requests-kerberos>=0.12.0",
+        "pandas>=0.23.4",
+    ],
     classifiers=[
-            "Intended Audience :: Information Technology",
-            "Intended Audience :: System Administrators",
-            "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
     ],
 )
